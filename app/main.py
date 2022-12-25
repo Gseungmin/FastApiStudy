cc049bb86b85602770d66de67efa6a2e9aef4f3d
Ji Seungmin <cswcsm02@gmail.com>
2022-12-25 22:18:56 +0900

    FastApi Study완료

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from app.book_scraper import NaverBookScraper
 from app.models import mongodb
 from app.models.book import BookModel
 
@@ -25,14 +26,40 @@ templates = Jinja2Templates(directory=BASE_DIR/"templates")
 #index.html에 request와 id 변수에 값 지정
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    book = BookModel(keyword="파이썬", publisher="public", price=1200, image='me.png')
-    print(await mongodb.engine.save(book)) #save 함수가 async함수 이므로 즉 코루틴 함수 이므로 await을 붙임
+    # book = BookModel(keyword="파이썬", publisher="public", price=1200, image='me.png')
+    # print(await mongodb.engine.save(book)) #save 함수가 async함수 이므로 즉 코루틴 함수 이므로 await을 붙임
     return templates.TemplateResponse("./index.html", {"request": request, "title": "콜렉터"})
 
 #q를 통해 검색어 받기, 데이터를 받아 MongoDB에 저장
 @app.get("/search", response_class=HTMLResponse)
 async def search(request: Request, q:str):
-    return templates.TemplateResponse("./index.html", {"request": request, "keyword": q})
+    #쿼리에서 검색어 추출
+    keyword = q
+    #검색어가 없다면 검색 요구 리턴
+    if not keyword:
+        return templates.TemplateResponse("./index.html", {"request": request})
+
+    #해당 검색어의 데이터가 이미 DB에 존재한다면 그냥 보여줌
+    if await mongodb.engine.find_one(BookModel, BookModel.keyword == keyword):
+        #만약 존재한다면 모든 데이터 가지고 와서 뷰 구성
+        books = await mongodb.engine.find(BookModel, BookModel.keyword == keyword)
+        return templates.TemplateResponse("./index.html", {"request": request, "books": books})
+
+    #Scrapper를 통해 해당 검색어에 대한 데이터 수집
+    naver_book_scrapper = NaverBookScraper()
+    #search가 비동기함수이므로 await 설정, 총 10개의 데이터를 가지고 옴
+    books = await naver_book_scrapper.search(keyword, 10)
+    #각 모델 생성
+    book_models = []
+    for book in books:
+        book_model = BookModel(keyword = keyword,
+                               publisher=book["publisher"],
+                               price=book["discount"], image=book["image"])
+        book_models.append(book_model)
+    #DB에 수집된 데이터 저장
+    # save_all함수는 await의 동기 패턴을 asyncio 대신 사용해 동시 실행 패턴으로 변경
+    await mongodb.engine.save_all(book_models) # save 함수가 async함수 이므로 즉 코루틴 함수 이므로 await을 붙임
+    return templates.TemplateResponse("./index.html", {"request": request, "books": books})
 
 # DB와 연결하고 서버가 끝날때 DB와 연결 해제
 # app이 시작될때 함수 실행
